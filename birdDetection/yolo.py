d7b1af221aee8bbfe71a158e6e579160b59362fd
Malcolm Benedict <mmbenedi@mtu.edu>
2026-02-25 22:38:46 -0500

    working demo wih laptop webcam

--- birdDetection/yolo.py
+++ birdDetection/yolo.py
@@ -12,10 +12,16 @@ def getColors(cls_num):
     return tuple(random.randint(0, 255) for _ in range(3))
 
 video_path = "videos/Mallard.mp4"
-videoCap = cv2.VideoCapture(video_path)
 
-fourcc = cv2.VideoWriter.fourcc(*'MPEG')  # Codec
-out = cv2.VideoWriter('output.mp4', fourcc, 24, (1280, 720))
+
+videoCap = cv2.VideoCapture(0)
+if not videoCap.isOpened():
+    print("Error: Cannot open video capture")
+    exit()
+frame_width = int(videoCap.get(cv2.CAP_PROP_FRAME_WIDTH))
+frame_height = int(videoCap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+fourcc = cv2.VideoWriter.fourcc(*'mp4v')  # Codec
+out = cv2.VideoWriter('output.mp4', fourcc, 30, (frame_width, frame_height))
 
 while videoCap.isOpened():
     ret, frame = videoCap.read()
@@ -43,7 +49,9 @@ while videoCap.isOpened():
                 points = np.hstack(track).astype(np.int32).reshape((-1, 1, 2))
                 color = getColors(track_id)
                 cv2.polylines(frame, [points], isClosed=False, color=color, thickness=2)
+    cv2.imshow('Camera',frame)
     out.write(frame)
+    cv2.waitKey(1)
 videoCap.release()
 out.release()
 cv2.destroyAllWindows()
