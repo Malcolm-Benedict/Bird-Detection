e218a8198dac79939d7bbef18e68fde66e44e95e
Malcolm Benedict <mmbenedi@mtu.edu>
2026-03-07 18:26:29 -0500

    minor changes to main

--- birdDetection/main.py
+++ birdDetection/main.py
@@ -9,6 +9,33 @@ import os
 import atexit
 import random
 
+def gstreamer_pipeline(
+    sensor_id=0,
+    capture_width=1920,
+    capture_height=1080,
+    display_width=960,
+    display_height=720,
+    framerate=30,
+    flip_method=0,
+):
+    return (
+        "nvarguscamerasrc sensor-id=%d ! "
+        "video/x-raw(memory:NVMM), width=(int)%d, height=(int)%d, framerate=(fraction)%d/1 ! "
+        "nvvidconv flip-method=%d ! "
+        "video/x-raw, width=(int)%d, height=(int)%d, format=(string)BGRx ! "
+        "videoconvert ! "
+        "video/x-raw, format=(string)BGR ! appsink"
+        % (
+            sensor_id,
+            capture_width,
+            capture_height,
+            framerate,
+            flip_method,
+            display_width,
+            display_height,
+        )
+    )
+    
 MODEL_PATH = 'models/'
 OUTPUT_PATH = 'outputs/'
 VIDEO_PATH = 'videos/'
@@ -44,10 +71,13 @@ elif source == "video":
 elif source == "gstreamer": # incorrect syntax
     with open(CONFIG_PATH+args.config_path, 'r') as file:
         cfg = yaml.safe_load(file)
-        videoCap = cv2.VideoCapture(cfg,cv2.CAP_GSTREAMER)
+        pipe = gstreamer_pipeline()
+        print(pipe)
+        videoCap = cv2.VideoCapture(gstreamer_pipeline(),cv2.CAP_GSTREAMER)
 else:
     print("Please specify video source!")
     exit()
+
 
 def exit_handler():
     videoCap.release()
--- birdDetection/tracker.py
+++ birdDetection/tracker.py
@@ -6,9 +6,11 @@ from collections import defaultdict
 
 class YoloTracker:
     def __init__(self,model):
-        self.model = YOLO(model)
+        self.model = YOLO(model, task="detect")
         self.track_history = defaultdict(lambda: [])
 
     def get_results(self,frame):
         results = self.model.track(frame, persist=True)
         return results
+    def make_engine(self):
+        self.model.export(format="engine")
